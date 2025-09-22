1f61a08e1130a216e7aca439187dcaaa3df432c4
crizostomo <diogo_crizostomo@hotmail.com>
2025-09-22 19:45:45 -0300

    Updating TreasuryRatesDTO

--- src/main/java/apitransactiontreasuryconv/govtreasuryapi/TreasuryApiRestConfig.java
+++ src/main/java/apitransactiontreasuryconv/govtreasuryapi/TreasuryApiRestConfig.java
@@ -5,6 +5,9 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.client.RestTemplate;
 
+/***
+ * Configuration problem: @Configuration class 'TreasuryApiRestConfig' may not be final.
+ */
 @Configuration
 public class TreasuryApiRestConfig {
 
